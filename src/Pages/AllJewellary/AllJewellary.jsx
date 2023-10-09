5b3e93ceee0bc3c56e20e40835b1640f270f349d
Abdur-P-Rahim <bspirahim@gmail.com>
2023-10-09 13:19:08 +0600

    responsive banner ok

--- src/Pages/AllJewellary/AllJewellary.jsx
+++ src/Pages/AllJewellary/AllJewellary.jsx
@@ -8,7 +8,7 @@ const AllJewellary = () => {
     useTitle('All Jewellary')
     return <>
         <h2 className='text-5xl text-center my-8'>All Jewellarys</h2>
-        <div class="grid grid-cols-2 lg:grid-cols-5 md:grid-cols-3 gap-4 md:px-20 my-8">{
+        <div class="grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 gap-4 md:px-20 my-8">{
             jewellarys.length > 0 && jewellarys.map(jewellary => <Product
                 key={jewellary._id}
                 jewellary={jewellary}
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -5,15 +5,15 @@ const Banner = () => {
     return (<div className='max-h-screen'>
         <div className="relative w-full">
             <img src='/slideshow1-2.jpg' className="w-full" />
-            <div className="absolute h-full flex items-center right-0 bottom-0">
-                <div className='space-y-7 pr-20 ml-16 text-left'>
-                    <div className='flex'>
-                        <div className='text-5xl font-bold'>Diamonds<br />Jewellary Collection
+            <div className="absolute h-full flex items-center right-2 bottom-0">
+                <div className='lg:space-y-7  lg:mr-16 lg:pr-5   text-right'>
+                    <div className=''>
+                        <div className='lg:text-5xl md:text-xl font-bold '>Diamonds<br />Jewellary Collection
                         </div>
                     </div>
-                    <p className='text-2xl font-bold'>Shukra Yogam & Silver Power Silver Saving Schemes.</p>
+                    <p className='lg:text-2xl md:xl font-bold lg:block hidden'>Shukra Yogam & Silver Power Silver Saving Schemes.</p>
                     <button>
-                        <Link to={'/jewellarys'}><button class="primary-btn">SHOP NOW</button></Link>
+                        <Link to={'/jewellarys'}><button className="primary-btn">SHOP NOW</button></Link>
                     </button>
                 </div>
             </div>
